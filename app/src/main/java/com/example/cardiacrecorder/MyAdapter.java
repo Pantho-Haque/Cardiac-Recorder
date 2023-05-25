31ed0f5380e2471a1cd482e7d65afb622c1cc4d1
Shahana1907029 <shahanaakter02000@gmail.com>
2023-05-25 12:43:57 +0600

    Deleting entry

--- app/src/main/java/com/example/cardiacrecorder/MyAdapter.java
+++ app/src/main/java/com/example/cardiacrecorder/MyAdapter.java
@@ -1,16 +1,26 @@
 package com.example.cardiacrecorder;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -49,6 +59,31 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             @Override
             public void onClick(View v) {
 
+                ProgressDialog loader=new ProgressDialog(context);
+                loader.setMessage("Deleting The Entry...");
+                loader.setCanceledOnTouchOutside(true);
+                loader.show();
+
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                //  onlineUserId = myAuth.getCurrentUser().getUid();
+                String onlineUserId="12345";
+                DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("entries").child(onlineUserId);
+
+                reference.child(onlineUserId).removeValue()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task <Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(context, "Task has been Deleted successfully", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    String err = task.getException().toString();
+                                    Toast.makeText(context, "Failed: " + err, Toast.LENGTH_SHORT).show();
+                                }
+                                loader.dismiss();
+                            }
+                        });
             }
         });
 
